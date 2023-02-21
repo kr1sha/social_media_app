fb3065d570bd1c1ff0e47d967997167913cfb4b8
kr1sha <elustov@mail.ru>
2023-02-21 07:36:51 +0300

    add signup

---
+++ media_app/core/modules.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.core.exceptions import ValidationError
+
+
+def find_user_validation_errors(username: str, email: str, password: str, password2: str) -> list:
+    """ Returns a list of errors that occur when trying to create a user with the current data """
+    errors = []
+
+    if password != password2:
+        errors.append('Password not matching')
+
+    if User.objects.filter(email=email).exists():
+        errors.append('Email taken')
+
+    if User.objects.filter(username=username).exists():
+        errors.append('Username taken')
+
+    return errors
+
+
+def create_user(username: str, email: str, password: str, password2: str) -> User:
+    """ Returning created user or raising validation error """
+    errors = find_user_validation_errors(username, email, password, password2)
+    if errors:
+        raise ValidationError(errors)
+    else:
+        user = User.objects.create_user(username, email, password)
+        user.save()
+        return user
+
--- media_app/core/views.py
+++ media_app/core/views.py
@@ -1,6 +1,36 @@
+from django.core.exceptions import ValidationError
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+
+from .modules import create_user
+from .models import Profile
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def signup(request):
+
+    if request.method == 'POST':
+
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        try:
+            user = create_user(username, email, password, password2)
+        except ValidationError as e:
+            [messages.info(request, error) for error in e]
+            return redirect('signup')
+
+        profile = Profile.objects.create(user=user, id_user=user.id)
+        profile.save()
+
+        redirect('login')
+
+    else:
+        return render(request, 'signup.html')
